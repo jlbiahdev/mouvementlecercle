f2b2b8987b55a846f49878da1f65b5945ecda063
jlbiahdev <jeanlucbiah.dev@outlook.com>
2025-05-13 18:40:29 +0200

    Add fields to subscription form

--- src/public/js/mail.js
+++ src/public/js/mail.js
@@ -1,15 +1,35 @@
+function resetControls() {
+  console.log('resetControls');
+  let fullname = document.getElementById('fullname');
+  let email = document.getElementById('email');
+  let phone = document.getElementById('phone');
+
+  fullname.value = '';
+  email.value = '';
+  phone.value = '';
+}
+
 function sendEmail() {
   console.log('sendEmail');
+  let form = document.getElementById('subscribe-form');
+
+  form.classList.remove('form-error');
+
+  let fullname = document.getElementById('fullname').value;
   let email = document.getElementById('email').value;
+  let phone = document.getElementById('phone').value;
 
   let params = {
       to_name: 'MLC Support',
       from_name: 'MLC curious',
-      from_email: 'jlbiah@gmail.com',
+      from_email: 'admin@cyrano-conseil.com',
       from_phonenumber: '',
-      message: 'notify me : ' + email,
-      subject: 'notify me Alert for MLC',
-      reply_to: 'jlbiah@gmail.com',
+      message: 'email : ' + email + '\n' + 
+                'fullname: ' + fullname + '\n' +
+                'phone: ' + phone + '\n' +
+                'message: I am interested in MLC and would like to be notified when it is available.',
+      subject: 'Alert for MLC',
+      reply_to: 'admin@cyrano-conseil.com,mouvementlecercle2050@gmail.com',
   }
 
   //Create an emailjs account and set your parameters here
@@ -25,10 +45,16 @@ function sendEmail() {
       data: JSON.stringify(emailjsConfig),
       contentType: 'application/json'
   }).done(function() {
-      alert('Votre mail a été envoyé!');
-      // resetControls();
+    form.classList.add('form-success');
+    let successmessage = document.getElementById('success-message');
+    successmessage.innerHTML = 'Your subscription has been registered';
+    resetControls();
   }).fail(function(error) {
       console.log('Oops... ' + JSON.stringify(error));
-      alert(`Un problème est survenu lors de l'envoi de votre mail`)
+      let errormessage = document.getElementById('error-message');
+      let errorText = "A problem occurred while processing your subscription.";
+      errormessage.innerHTML = errorText;
+      form.classList.add('form-error');
+      // alert(`Un problème est survenu lors de l'envoi de votre mail`)
   });
 }
